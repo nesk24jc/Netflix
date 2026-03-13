24d40e5d956ad6de94ae7faa248ef0270f115b2e
João Santos <joao.santos@etu.unilim.fr>
2026-03-13 17:01:39 +0100

    refactor(auth): Use AuthProvider in ProtectedRoute

--- frontend-user/src/utils/ProtectedRoute.jsx
+++ frontend-user/src/utils/ProtectedRoute.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthProvider';
 
 function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
 
-    
-  const isAuthenticated = localStorage.getItem('user') !== null;
-
-  if (!isAuthenticated) {
-
+  if (!isAuthenticated()) {
     return <Navigate to="/login" replace />;
   }
 
-  
   return children;
 }
 
