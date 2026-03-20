249fd3f9e07dffaafcd59607dca0b21419c52df6
João Santos <joao.santos@etu.unilim.fr>
2026-03-20 15:13:57 +0100

    feat: Integrate CartProvider into App structure and add cart route

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -5,21 +5,31 @@ import MyRentals from './pages/MyRentals';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
+
+
 
 import ProtectedRoute from './utils/ProtectedRoute';
 import { AuthProvider } from './context/AuthProvider'; 
+import { CartProvider } from './context/CartContext';
+
 
 function App() {
    return (
       
-      <AuthProvider>
+   
          <BrowserRouter>
+            <AuthProvider>
+               <CartProvider>
+
             <Routes>
                {/* Routes publiques */}
                <Route path="/" element={<Home />} />
                <Route path="/movie/:id" element={<MovieDetail />} />
                <Route path="/login" element={<Login />} />
                <Route path="/register" element={<Register />} />
+               <Route path="/cart" element={<Cart />} />
+               
                
                {/* Route protégée (Nécessite d'être connecté) */}
                <Route
@@ -34,8 +44,12 @@ function App() {
                {/* Route 404 (Toujours à mettre en tout dernier) */}
                <Route path="*" element={<NotFound />} />
             </Routes>
+            </CartProvider>
+            </AuthProvider>
          </BrowserRouter>
-      </AuthProvider>
+
+
+      
    ); 
 }
 
